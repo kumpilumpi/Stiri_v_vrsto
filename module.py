56eb6f9772a59edbe36dcb6cec349438d4336d0b
Jaka Kump <jakakump123@gmail.com>
2019-12-12 23:55:33 +0100

    Katerer funkcije bi mogel napisat

--- module.py
+++ module.py
@@ -1,2 +1,22 @@
 #stiri v vrsto
 
+class igra:
+    def __init__(self, poteza, mreza = None):
+        self.poteza = 'O'
+
+        if mreza is None: 
+            self.mreza = [['-','-','-','-','-','-', '-'] for _ in range(6)]
+        else: 
+            self.mreza = mreza
+
+        def stiri_v_vrsto(self):
+            '''Preveri, če so kje v mrezi 4 v vrsto'''
+            pass
+
+        def visina(self, stolpec):
+            '''Katera vrsta v določenem stolpcu je prazna'''
+            pass
+
+        def poteza(self, stolpec):
+            '''Naredi potezo'''
+            pass
