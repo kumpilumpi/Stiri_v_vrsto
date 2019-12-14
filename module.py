7384813953ececf1dcfc3ff193637fe24a490a2c
Jaka Kump <jakakump123@gmail.com>
2019-12-14 15:17:13 +0100

    Fun stiri v vrsto seznami Nov način kako poiskati 4 v vrsto

--- module.py
+++ module.py
@@ -3,13 +3,35 @@
 class igra:
     def __init__(self, mreza = None):
         self.navrsti = 'O'
+        self.zadnja_poteza = (0,0) #(visina, stolpec)
 
         if mreza is None: 
-            self.mreza = [['-','-','-','-','-','-', '-'] for _ in range(6)]
+            self.mreza = [['-','-','-','-','-','-', '-'] for _ in range(6)] #seznami aso narobe obrnjeni od vzgoraj navzdol je za igrt
         else: 
             self.mreza = mreza
 
+    def stiri_linija_seznami(self):
+        '''Vrne vse sezname v katerih je lahko 4 v vrsto'''
+        sez_r_diagonala = []
+        sez_p_diagonala = []
+        sez_visina = []
+        for i in range(-3,4): #linija
+            try:
+                sez_r_diagonala.append(self.mreza[self.zadnja_poteza[0] + i ][self.zadnja_poteza[1] + i ])
+            except: pass
+            try:
+                sez_p_diagonala.append(self.mreza[self.zadnja_poteza[0] - i ][self.zadnja_poteza[1] - i ])
+            except: pass
+            try:
+                sez_visina.append(self.mreza[self.zadnja_poteza[0] - i ][self.zadnja_poteza[1]])
+            except: pass
+
+        return [sez_r_diagonala, sez_p_diagonala, sez_visina, self.mreza[self.zadnja_poteza[0]]]
+
+
     def stiri_v_vrsto(self): #Mogoče dodam zadnja poteza var,da je mnj za pregledovt, če je konc
+        #Try: Except IndexError
+        #dodatna spremenljivka seznam seznamov, v katerih preveri, če so notr 4 v vrsto
         '''Preveri, če so kje v mrezi 4 v vrsto'''
         pass
 
@@ -26,7 +48,8 @@ class igra:
 
     def poteza(self, stolpec):
         '''Naredi potezo'''
-        self.mreza[self.visina(stolpec)][stolpec] = self.navrsti
+        self.zadnja_poteza = (self.visina(stolpec), stolpec)
+        self.mreza[self.zadnja_poteza[0]][stolpec] = self.navrsti
         self.navrsti = self.naslednji()
         return
 
@@ -37,5 +60,9 @@ class igra:
 # igra.poteza(3)
 # print(igra.navrsti)
 # print(igra.mreza)
+# igra.poteza(3)
+# print(igra.mreza)
+# igra.poteza(3)
+# igra.poteza(3)
 # igra.poteza(3)
 # print(igra.mreza)
